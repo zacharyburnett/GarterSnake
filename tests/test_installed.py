0a68009db079193312be51dfe148c64dc09b847f
zacharyburnett <zachary.r.burnett@gmail.com>
2022-02-26 13:04:26 -0500

    tests

--- tests/test_installed.py
+++
@@ -1,8 +0,0 @@
-from gartersnake.pip import installed_packages
-
-
-def test_installed_packages():
-    packages = installed_packages()
-
-    reference_packages = ['dunamai', 'pip', 'setuptools']
-    assert all(package in packages for package in reference_packages)
--- tests/test_missing.py
+++
@@ -1,9 +0,0 @@
-from gartersnake.missing import missing_requirements
-
-
-def test_check_missing_requirements():
-    test_packages = ['dunamai', 'requests']
-
-    missing_packages = missing_requirements(test_packages)
-
-    assert missing_packages == ['requests']
