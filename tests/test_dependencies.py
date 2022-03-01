610f10f3e452a03c8b805a2c8c993470210fad86
zacharyburnett <zachary.r.burnett@gmail.com>
2022-03-01 11:49:33 -0500

    test dependency tree

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -4,4 +4,4 @@ from gartersnake.dependencies import dependency_tree
 def test_dependency_tree():
     tree = dependency_tree('requests', 'numpy', 'pyproj', 'adcircpy')
 
-    tree
+    assert list(tree) == ['adcircpy']
